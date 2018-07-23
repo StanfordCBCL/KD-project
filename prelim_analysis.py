b36e78c6a2ce7a72ea97c6a1f8189213d282b79e
Alex Lu <alu2@stanford.edu>
2018-07-23 12:10:48 -0700

    argparse to allow for smooth transition of some computation into cluster

--- prelim_analysis.py
+++ prelim_analysis.py
@@ -3,10 +3,9 @@
 '''
 
 import numpy as np
-import vtk
-from vtk.util import numpy_support as nps
 import sys
 from AneurysmGeneration.utils.batch import *
+import argparse
 
 
 def extract_points(polydata):
@@ -30,6 +29,7 @@ def return_polydata(path):
 		Given a file name, open and return the data
 	'''
 	print 'return polydata'
+	import vtk
 
 	reader = vtk.vtkXMLPolyDataReader()
 	reader.SetFileName(path)
@@ -43,11 +43,17 @@ def parse_command_line(args):
 	
 	print 'parsing command line'
 
-	if len(args) == 1: 
-		return ("AneurysmGeneration/models/SKD0050/SKD0050_baseline_model_modified_p1.vtp", "Artificial/RCA/p1/all_results.vtp")
+	parser = argparse.ArgumentParser(description='lol')
+	parser.add_argument('--source', action="store", type=str, default="AneurysmGeneration/models/SKD0050/SKD0050_baseline_model_modified_p1.vtp")
+	parser.add_argument('--results', action="store", type=str, default="Artificial/RCA/p1/all_results.vtp")
+	parser.add_argument('--pkl', dest='feature', action='store_true')
+	parser.add_argument('--vtk', dest='feature', action='store_true')
+	parser.add_argument('--suff', action="store", type=str, default='p1')
+	parser.set_defaults(feature=True)
+	
+	args = vars(parser.parse_args())
 
-	else: 
-		return (args[1], args[2])
+	return args
 
 def apply_bounding_box(centerline, points, offsets=np.array([.07, .07, .07])):
 	'''
@@ -79,47 +85,105 @@ def apply_bounding_box(centerline, points, offsets=np.array([.07, .07, .07])):
 	return patch_index
 
 
+def prepare_chunks(idx, block_sz=100):
+	'''
+		input: 
+			* idx, an np array of indices 
+			* block_sz (optiona), the size of each partitioning block
+
+	'''
+	print 'preparing chunks'
+
+	total = len(idx)
+	print total
+	partitions = block_sz*np.arange(total//block_sz)[1:]
+	
+	print partitions
+	return partitions
+	# return np.split(idx, partitions)
+
+
 def min_dist(points_results, points_source):
 
 	distances = np.sqrt(((points_results - points_source[:, np.newaxis])**2).sum(axis=2))
 	return np.argmin(distances, axis=0)
 
+
+def write_points_to_pkl(points_source, points_results, suff):
+	write_to_file('points_' + suff, (points_source, points_results))
+
+
 def main():
 
-	source_model_path, all_results_path = parse_command_line(sys.argv)
+
+	CONTINUE=False
+	args = parse_command_line(sys.argv)
 
 	# # we need the face to points correspondence from the original source model 
 	# # we only need to get this once 
 	(face_to_points, _, _, NoP) = read_from_file("big_boy")
 
-	# # we need to get the source models
-	# source_model_base_path = 'AneurysmGeneration/models/SKD0050/SKD0050_baseline_model_modified_p1.vtp'
-	poly_source = return_polydata(source_model_path)
+	points_source = None
+	points_results = None
 
-	# # we need the all_results_mesh
-	# all_results_base_path = 'Artificial/RCA/p1/all_results.vtp'
-	poly_results = return_polydata(all_results_path)
-	
-	gNid_array = nps.vtk_to_numpy(poly_results.GetPointData().GetArray('GlobalNodeID'))
-	print 'the gnid array has shape', gNid_array.shape
+	if args['feature']: 
+		source_model_path = args['source']
+		all_results_path = args['results']
 
-	# # extract the raw points from the polydata
-	points_source = extract_points(poly_source)
-	points_results = extract_points(poly_results)
+		# we need to get the source models
+		# source_model_base_path = 'AneurysmGeneration/models/SKD0050/SKD0050_baseline_model_modified_p1.vtp'
+		poly_source = return_polydata(source_model_path)
 
-	# # load in the centerline
-	centerline = read_from_file('RCA_cl')
-	
-	# use the centerline to apply a bounding box to the points that we need to look at 
-	bounded_source_idx = apply_bounding_box(centerline, points_source)
-	bounded_results_idx = apply_bounding_box(centerline, points_results)
+		# we need the all_results_mesh
+		# all_results_base_path = 'Artificial/RCA/p1/all_results.vtp'
+		poly_results = return_polydata(all_results_path)
 
-	print points_results.shape
-	print bounded_results_idx[0].shape
+		from vtk.util import numpy_support as nps	
+		gNid_array = nps.vtk_to_numpy(poly_results.GetPointData().GetArray('GlobalNodeID'))
 
-	print points_source.shape
-	print bounded_source_idx[0].shape
+		print 'the gnid array has shape', gNid_array.shape
 
+		# extract the raw points from the polydata
+		points_source = extract_points(poly_source)
+		points_results = extract_points(poly_results)
+
+		write_points_to_pkl(points_source, points_results, args['suff'])
+
+	else:
+		'reading points from pickle'
+		points_source, points_results = read_from_file('points_' + suff)
+
+
+	if CONTINUE:
+		# # load in the centerline
+		centerline = read_from_file('RCA_cl')
+		
+		# use the centerline to apply a bounding box to the points that we need to look at 
+		bounded_source_idx = apply_bounding_box(centerline, points_source)[0]
+		bounded_results_idx = apply_bounding_box(centerline, points_results)[0]
+
+		print points_results.shape
+		print bounded_results_idx.shape
+
+		print points_source.shape
+		print bounded_source_idx.shape
+
+		# chunk one of the arrays so that we don't run out of memory when we 
+		# perform the vectorized distance computation
+
+		#partitions = prepare_chunks(bounded_results_idx) 
+		mapping = np.zeros(points_results.shape[0])
+
+		block_sz = 100
+		for i in range(len(bounded_results_idx)//100):
+		# for c,  in enumerate(partitions[:10]):
+			print 'split', i
+			#print partition.dtype
+			#print chunk.shape
+			cur_idx = bounded_results_idx[block_sz*i:block_sz*(i+1)]
+			mapping[cur_idx] = min_dist(points_results[cur_idx], points_source)
+
+			# print chunk.shape
 
 	
 	
