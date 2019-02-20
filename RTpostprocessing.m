bee25216823a5b36ffb135ce202906d636a2f5de
Alex Lu <alu2@stanford.edu>
2019-02-19 17:04:55 -0800

    small changes, preparing to post process, visualize RT1 simulations

---
+++ RTpostprocessing.m
@@ -0,0 +1,23 @@
+close all
+clear all
+
+dt = 0.0007717 ;
+time = dt:dt:(dt*1000) ; 
+path = [ '~/Documents/lab/KD-project/AdvectionDiffusion/right/asi2/p1/RT1/' ] ;
+
+    
+volume_integral = load( [path , 'int_r.dat'] );
+volume = load( [path,'Volume.dat'] ) ;
+
+figure
+plot(volume_integral(:,end)) ;
+
+l=length(volume_integral(:,1)) ;
+
+time_integral = trapz(time ,volume_integral(l-1000 + 1:end,2) ) ;
+RT1 = time_integral/volume ;
+
+norm_volume_integral = volume_integral/volume - 1 ;
+norm_RT1 = trapz(time ,norm_volume_integral(l-1000 + 1:end,2) ) ;
+
+display(['RT1 = ', num2str(norm_RT1), ' [s]'])
