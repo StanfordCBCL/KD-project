made a bunch of attempts to debug theta computation. fixed and piped trimming of vtus, which was erroneous before :/ rerunning distal rca asi2 cases for all z-scores

AneurysmGeneration/generate.py
@@ -62,7 +62,7 @@ def grow_aneurysm(wall_name, face_to_points, point_to_face, face_to_cap, point_c
 
 	centerline=read_from_file('RCA_cl')
 
-	wall_ref, normalized_center, wall_to_center, min_dists, centerline_length = projection(NoP_wall, centerline, wall_points, np.array(list(included_points)))
+	wall_ref, normalized_center, wall_to_center, min_dists, centerline_length = projection(NoP_wall, centerline, wall_points, np.arange(NoP_wall))# np.array(list(included_points)))
 
 	# compute the normalized length -> normalized end position
 	end=start+length/centerline_length
@@ -88,38 +88,38 @@ def grow_aneurysm(wall_name, face_to_points, point_to_face, face_to_cap, point_c
 
 	expand_np = np.zeros((wall_model.GetNumberOfPoints()))
 
-	for i in range(NoP_wall): 
-		if i in wall_region:
-			expand_np[i] = 1
+	# for i in range(NoP_wall): 
+	# 	if i in wall_region:
+	# 		expand_np[i] = 1
 			
-	for i, pointID in enumerate(wall_region):
+	# for i, pointID in enumerate(wall_region):
 
-		cur_pt = wall_model.GetPoints().GetPoint(pointID)
-		wall_normal = [r1 - r2 for (r1, r2) in zip(cur_pt, wall_to_center[pointID]) ]
+	# 	cur_pt = wall_model.GetPoints().GetPoint(pointID)
+	# 	wall_normal = [r1 - r2 for (r1, r2) in zip(cur_pt, wall_to_center[pointID]) ]
 
-		displace = []
-		new_pt = []
+	# 	displace = []
+	# 	new_pt = []
 
-		if expansion_mode == 'scalar':
-			displace = [expand[i]*dn for (r, dn) in zip(cur_pt, normal)]
-			new_pt = [r + dr for (r, dr) in zip(cur_pt, displace)]
+	# 	if expansion_mode == 'scalar':
+	# 		displace = [expand[i]*dn for (r, dn) in zip(cur_pt, normal)]
+	# 		new_pt = [r + dr for (r, dr) in zip(cur_pt, displace)]
 			
 
-		elif expansion_mode == 'absolute':
+	# 	elif expansion_mode == 'absolute':
 
-			if expand[i] < min_dists[pointID]: 
-				expand[i] = min_dists[pointID]
+	# 		if expand[i] < min_dists[pointID]: 
+	# 			expand[i] = min_dists[pointID]
 
-	 		wall_unit = [xi/min_dists[pointID] for xi in wall_normal]
-			displace = [r*expand[i] for r in wall_unit]
-			new_pt = [r + dr for (r, dr) in zip(wall_to_center[pointID], displace)]
+	#  		wall_unit = [xi/min_dists[pointID] for xi in wall_normal]
+	# 		displace = [r*expand[i] for r in wall_unit]
+	# 		new_pt = [r + dr for (r, dr) in zip(wall_to_center[pointID], displace)]
 
-		# after applying the displacement to the wall points, modify displacement magnitude for branch shift
-		displace_adjusted = [d - n for (d, n) in zip (displace, wall_normal)]
+	# 	# after applying the displacement to the wall points, modify displacement magnitude for branch shift
+	# 	displace_adjusted = [d - n for (d, n) in zip (displace, wall_normal)]
 
-		for face, points in intersect.iteritems():
-			if pointID in points:
-				affected_face_displace[face].append(np.array(displace_adjusted))
+	# 	for face, points in intersect.iteritems():
+	# 		if pointID in points:
+	# 			affected_face_displace[face].append(np.array(displace_adjusted))
 
 
 		# alter the current point's coordinates to reflect expansion
@@ -131,7 +131,7 @@ def grow_aneurysm(wall_name, face_to_points, point_to_face, face_to_cap, point_c
 	print '>>>  the maximum displacement: ', np.max(expand_np)
 	print '>>>  the wall ref shape: ', wall_ref.shape
 
-	shift_branches(wall_model, wall_region, intersect, originals, affected_face_displace, face_to_cap, face_to_points, point_connectivity, easing)
+	# shift_branches(wall_model, wall_region, intersect, originals, affected_face_displace, face_to_cap, face_to_points, point_connectivity, easing)
 	
 	# add min dist to vtk file, for debugging purposes
 	min_dist_vtk = nps.numpy_to_vtk(min_dists)

AneurysmGeneration/utils/normalization.py
@@ -213,7 +213,8 @@ def projection(NoP, centerline, wall_points, included_ids):
 	wall_to_center = {}
 	min_dists = np.zeros(NoP)
 
-	chunk_sz = 500
+	chunk_sz = 400
+	print len(included_ids)
 	n_chunks = len(included_ids)//chunk_sz
 
 	for c in tqdm(range(n_chunks), desc='splitting projection computation', file=sys.stdout):

prelim_analysis.py
@@ -28,6 +28,7 @@ from AneurysmGeneration.utils.batch import *
 from AneurysmGeneration.utils.normalization import *
 from AneurysmGeneration.utils.slice import *
 from AdvectionDiffusion.ad_utils import produce_tagfile
+from another_clipper import clip_side_branches_wrap
 
 
 def parse_command_line(args):
@@ -219,6 +220,7 @@ def post_process_clip(centerline, points_results, poly_results,
 					outdir, 
 					suff,
 					dry, 
+					vessel='RCA', shape='ASI4', 
 					save_parameters=True): 
 	"""Summary
 	
@@ -249,22 +251,22 @@ def post_process_clip(centerline, points_results, poly_results,
 
 	# ------------- potential debugging -------------------
 
-	mapped = read_from_file('debug_mapping_' + suff)
-	mapped_vtk = nps.numpy_to_vtk(mapped)
-	mapped_vtk.SetName('Mapped')
-	poly_results.GetPointData().AddArray(mapped_vtk)
+	# mapped = read_from_file('debug_mapping_' + suff)
+	# mapped_vtk = nps.numpy_to_vtk(mapped)
+	# mapped_vtk.SetName('Mapped')
+	# poly_results.GetPointData().AddArray(mapped_vtk)
 
-	wall_ref_transpose = np.transpose(wall_ref).copy()
+	# wall_ref_transpose = np.transpose(wall_ref).copy()
 
-	# add the normalized axial position wall array to the vtk file 
-	norm_wall_vtk = nps.numpy_to_vtk(wall_ref_transpose[0, :])
-	norm_wall_vtk.SetName('axial pos')
-	poly_results.GetPointData().AddArray(norm_wall_vtk)
+	# # add the normalized axial position wall array to the vtk file 
+	# norm_wall_vtk = nps.numpy_to_vtk(wall_ref_transpose[0, :])
+	# norm_wall_vtk.SetName('axial pos')
+	# poly_results.GetPointData().AddArray(norm_wall_vtk)
 
-	# add the normalized theta position wall array to the vtk file
-	theta_wall_vtk = nps.numpy_to_vtk(wall_ref_transpose[1, :])
-	theta_wall_vtk.SetName('theta')
-	poly_results.GetPointData().AddArray(theta_wall_vtk)
+	# # add the normalized theta position wall array to the vtk file
+	# theta_wall_vtk = nps.numpy_to_vtk(wall_ref_transpose[1, :])
+	# theta_wall_vtk.SetName('theta')
+	# poly_results.GetPointData().AddArray(theta_wall_vtk)
 
 
 	# ------------- isolate clipping boundaries -------------------
@@ -318,10 +320,16 @@ def post_process_clip(centerline, points_results, poly_results,
 	# ------------- connectivity to make sure we're extracting correctly -------------------
 	connect = vtk.vtkPolyDataConnectivityFilter()
 	connect.SetInputData(extract_end.GetOutput())
-	connect.SetExtractionModeToLargestRegion()
+	connect.SetExtractionModeToClosestPointRegion()
+	seed_id = wall_region[len(wall_region)/2]
+	x,y,z = poly_results.GetPoints().GetPoint(seed_id)
+	connect.SetClosestPoint(x,y,z)
+	# connect.SetExtractionModeToLargestRegion()
 	connect.Update()
 
 	region = connect.GetOutput()
+
+	# region = clip_side_branches_wrap(region, vessel, shape)
 
 	print 'output region has NoP:', region.GetNumberOfPoints()
 
@@ -337,10 +345,10 @@ def post_process_clip(centerline, points_results, poly_results,
 
 	print 'completed post_process_clip routine'
 
-	return (origin_start, origin_end, span)
+	return (origin_start, origin_end, span, (x,y,z))
 
 
-def clip_vtu(clip_parameters, outdir, shape, suff, unstructured_results, dry): 
+def clip_vtu(clip_parameters, outdir, shape, suff, unstructured_results, dry, vessel='RCA'): 
 	"""Summary
 	
 	Args:
@@ -352,9 +360,9 @@ def clip_vtu(clip_parameters, outdir, shape, suff, unstructured_results, dry):
 	print 'doing clip_vtu routine'
 
 	if isinstance(clip_parameters, str): 
-		origin_start, origin_end, span = read_from_file(clip_parameters)
+		origin_start, origin_end, span, (x,y,z) = read_from_file(clip_parameters)
 	else: 
-		origin_start, origin_end, span = clip_parameters
+		origin_start, origin_end, span, (x,y,z) = clip_parameters
 
 	# define planes
 	plane_start = vtk.vtkPlane()
@@ -379,12 +387,17 @@ def clip_vtu(clip_parameters, outdir, shape, suff, unstructured_results, dry):
 
 	connect = vtk.vtkConnectivityFilter()
 	connect.SetInputConnection(extract_end.GetOutputPort())
-	connect.SetExtractionModeToLargestRegion()
+	connect.SetExtractionModeToClosestPointRegion()
+	connect.SetClosestPoint(x,y,z)
 	connect.Update()
 
 	region = connect.GetOutput()
 
 	print region.GetNumberOfPoints()
+
+	# region = clip_side_branches_wrap(region, vessel, shape)
+
+	# print region.GetNumberOfPoints()
 
 	if not dry: 
 		clipped_writer = vtk.vtkXMLUnstructuredGridWriter()
@@ -462,7 +475,8 @@ def main():
 											NoP,
 											args['outdir'], 
 											args['suff'],
-											args['dry'])
+											args['dry'],
+											shape=args['shape'])
 					
 		if args['baseline']	: 
 			unstructured_results = return_unstructured(args['baseline_dir'])